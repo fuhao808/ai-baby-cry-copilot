666d3098ab707dde1ba56a2df48fee61c89b818b
fuhao808 <flyboy_808@hotmail.com>
2026-03-30 23:35:33 -0400

    Wire app to live Firebase project

--- frontend/lib/firebase_options.dart
+++ frontend/lib/firebase_options.dart
@@ -16,32 +16,52 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: String.fromEnvironment('FIREBASE_ANDROID_API_KEY',
-        defaultValue: 'demo-android-api-key'),
-    appId: String.fromEnvironment('FIREBASE_ANDROID_APP_ID',
-        defaultValue: '1:1234567890:android:demo'),
-    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID',
-        defaultValue: '1234567890'),
-    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID',
-        defaultValue: 'demo-baby-cry'),
-    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET',
-        defaultValue: 'demo-baby-cry.appspot.com'),
+    apiKey: String.fromEnvironment(
+      'FIREBASE_ANDROID_API_KEY',
+      defaultValue: 'AIzaSyDkN3C0Oba0YahDM9bTNZFOSJxCyR9rGH4',
+    ),
+    appId: String.fromEnvironment(
+      'FIREBASE_ANDROID_APP_ID',
+      defaultValue: '1:931948069020:android:709114faf98fe02fce2d59',
+    ),
+    messagingSenderId: String.fromEnvironment(
+      'FIREBASE_MESSAGING_SENDER_ID',
+      defaultValue: '931948069020',
+    ),
+    projectId: String.fromEnvironment(
+      'FIREBASE_PROJECT_ID',
+      defaultValue: 'baby-cry-ffa84',
+    ),
+    storageBucket: String.fromEnvironment(
+      'FIREBASE_STORAGE_BUCKET',
+      defaultValue: 'baby-cry-ffa84-cry-logs',
+    ),
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: String.fromEnvironment('FIREBASE_IOS_API_KEY',
-        defaultValue: 'demo-ios-api-key'),
-    appId: String.fromEnvironment('FIREBASE_IOS_APP_ID',
-        defaultValue: '1:1234567890:ios:demo'),
-    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID',
-        defaultValue: '1234567890'),
-    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID',
-        defaultValue: 'demo-baby-cry'),
-    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET',
-        defaultValue: 'demo-baby-cry.appspot.com'),
+    apiKey: String.fromEnvironment(
+      'FIREBASE_IOS_API_KEY',
+      defaultValue: 'AIzaSyBzSj215Fd4XkOFUMFA0KzPx1YazmGvJdU',
+    ),
+    appId: String.fromEnvironment(
+      'FIREBASE_IOS_APP_ID',
+      defaultValue: '1:931948069020:ios:fe7ce87c471ad75fce2d59',
+    ),
+    messagingSenderId: String.fromEnvironment(
+      'FIREBASE_MESSAGING_SENDER_ID',
+      defaultValue: '931948069020',
+    ),
+    projectId: String.fromEnvironment(
+      'FIREBASE_PROJECT_ID',
+      defaultValue: 'baby-cry-ffa84',
+    ),
+    storageBucket: String.fromEnvironment(
+      'FIREBASE_STORAGE_BUCKET',
+      defaultValue: 'baby-cry-ffa84-cry-logs',
+    ),
     iosBundleId: String.fromEnvironment(
       'FIREBASE_IOS_BUNDLE_ID',
-      defaultValue: 'com.example.aiBabyCryCopilot',
+      defaultValue: 'com.fuhao.aiBabyCryCopilot',
     ),
   );
 }
